a34aa8cf5d7de5bb306e24dc3243c7273ecb82d1
yaroslavpeshkov <159679641+yaroslavpeshkov@users.noreply.github.com>
2024-03-12 22:58:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # Case #3
 # Developers: Khramchikhina A., Peshkov Y., Sanzhanova A., Yurshenaite A.
 #
-import ru
+
+import ru_local as ru
 
 
 def income():
     '''
     The function determines the annual income amount.
+    :return: amount of income
     '''
     amount = 0
     for month in range(0, 12):
@@ -18,6 +20,7 @@ def income():
 def free_tax():
     '''
     The function determines the annual tax-free amount.
+    :return: amount of tax-free amount
     '''
     amount = 0
     for month in range(0, 12):
@@ -28,7 +31,9 @@ def free_tax():
 
 def subject(income_for_tax):
     '''
-    The function calculates the amount of the annual tax for a given entity.
+    The function calculates the amount of the annual tax for single subject.
+    :param income_for_tax: amount of taxable income
+    :return: amount of tax
     '''
     if income_for_tax < 9_076:
         tax = 0.1 * income_for_tax
@@ -46,18 +51,19 @@ def subject(income_for_tax):
         tax = 0.1*9_075 + 0.15*27_825 + 0.25*52_450 + 0.28*97_000 + 0.33*(income_for_tax - 186_350)
         return tax
     elif income_for_tax < 406_751:
-        tax = (0.1*9_075 + 0.15*27_825 + 0.25*52_450 + 0.28*97_000 + 0.33*218_750 +
-               0.35*(income_for_tax - 405_100))
+        tax = 0.1*9_075 + 0.15*27_825 + 0.25*52_450 + 0.28*97_000 + 0.33*218_750 + 0.35*(income_for_tax - 405_100)
         return tax
     else:
-        tax = (0.1*9_075 + 0.15*27_825 + 0.25*52_450 + 0.28*97_000 + 0.33*218_750 +
-               0.35*1_650 + 0.396*(income_for_tax - 406_750))
+        tax = 0.1*9_075 + 0.15*27_825 + 0.25*52_450 + 0.28*97_000 + 0.33*218_750 + 0.35*1_650 + \
+              0.396*(income_for_tax - 406_750)
         return tax
 
 
 def couple(income_for_tax):
     '''
-    The function calculates the amount of the annual tax for a given entity.
+    The function calculates the amount of the annual tax for married couple.
+    :param income_for_tax: amount of taxable income
+    :return: amount of tax
     '''
     if income_for_tax < 18_151:
         tax = 0.1 * income_for_tax
@@ -75,18 +81,19 @@ def couple(income_for_tax):
         tax = 0.1*18_150 + 0.15*55_650 + 0.25*75_050 + 0.28*78_000 + 0.33*(income_for_tax - 226_850)
         return tax
     elif income_for_tax < 457_601:
-        tax = (0.1*18_150 + 0.15*55_650 + 0.25*75_050 + 0.28*78_000 + 0.33*178_250 +
-               0.35*(income_for_tax - 405_100))
+        tax = 0.1*18_150 + 0.15*55_650 + 0.25*75_050 + 0.28*78_000 + 0.33*178_250 + 0.35*(income_for_tax - 405_100)
         return tax
     else:
-        tax = (0.1*18_150 + 0.15*55_650 + 0.25*75_050 + 0.28*78_000 + 0.33*178_250 + 0.35*52_500 +
-               0.396*(income_for_tax - 457_600))
+        tax = 0.1*18_150 + 0.15*55_650 + 0.25*75_050 + 0.28*78_000 + 0.33*178_250 + 0.35*52_500 + \
+              0.396*(income_for_tax - 457_600)
         return tax
 
 
 def parent(income_for_tax):
     '''
-    The function calculates the amount of the annual tax for a given entity.
+    The function calculates the amount of the annual tax for one parent.
+    :param income_for_tax: amount of taxable income
+    :return: amount of tax
     '''
     if income_for_tax < 12_951:
         tax = 0.1 * income_for_tax
@@ -104,16 +111,19 @@ def parent(income_for_tax):
         tax = 0.1*12_950 + 0.15*36_450 + 0.25*78_150 + 0.28*79_050 + 0.33*(income_for_tax - 206_600)
         return tax
     elif income_for_tax < 432_201:
-        tax = (0.1*12_950 + 0.15*36_450 + 0.25*78_150 + 0.28*79_050 + 0.33*198_500 +
-               0.35*(income_for_tax - 405_100))
+        tax = 0.1*12_950 + 0.15*36_450 + 0.25*78_150 + 0.28*79_050 + 0.33*198_500 + 0.35*(income_for_tax - 405_100)
         return tax
     else:
-        tax = (0.1*12_950 + 0.15*36_450 + 0.25*78_150 + 0.28*79_050 + 0.33*198_500 +
-               0.35*27_100 + 0.396*(income_for_tax - 432_200))
+        tax = 0.1*12_950 + 0.15*36_450 + 0.25*78_150 + 0.28*79_050 + 0.33*198_500 + 0.35*27_100 + \
+              0.396*(income_for_tax - 432_200)
         return tax
 
 
 def main():
+    '''
+    Main function.
+    :return: None
+    '''
     print(ru.CATEGORY, ru.SUBJECT, ru.COUPLE, ru.PARENT, sep='\n')
     category = int(input(ru.VALUE))
     print(ru.YEAR_INCOME)
@@ -124,6 +134,15 @@ def main():
     print(ru.TOTAL_FREE_TAX, total_free_tax)
     income_for_tax = total_income - total_free_tax
     print(ru.INCOME_FOR_TAX, income_for_tax)
+    if category == 1:
+        print(ru.YEAR_TAX, format(subject(income_for_tax), '.2f'))
+        print(ru.MONTH_PAYMENT, format(subject(income_for_tax) / 12, '.2f'))
+    elif category == 2:
+        print(ru.YEAR_TAX, format(couple(income_for_tax), '.2f'))
+        print(ru.MONTH_PAYMENT, format(couple(income_for_tax) / 12, '.2f'))
+    else:
+        print(ru.YEAR_TAX, format(parent(income_for_tax), '.2f'))
+        print(ru.MONTH_PAYMENT, format(parent(income_for_tax) / 12, '.2f'))
 
 
 if __name__ == '__main__':
